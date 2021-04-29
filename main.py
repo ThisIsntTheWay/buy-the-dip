79276b387c46bc6817f3c47b9b67a7c283dada1f
The man <ThisIsntTheWay@users.noreply.github.com>
2021-04-29 19:41:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 # bot.py
 import os
+import discord
+
+# Acquire token
+f = open("tokens\discord.txt","r")
+TOKEN = f.read()
 
 import discord
-from dotenv import load_dotenv
 
-load_dotenv()
-with open('.\tokens\discord.txt') as f:
-    TOKEN = f.readlines()
+# Discord client class
+class SniperGuy(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
 
-client = discord.Client()
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
 
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+client = SniperGuy()
 
+async def taskMsg():
+    await client.wait_until_ready()
+    bsc_channel = client.get_channel(837374595505455124)
+    await bsc_channel.send("Sniper ready for duty.")
+
+client.loop.create_task(taskMsg())
 client.run(TOKEN)
