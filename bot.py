58ab0aa44a68f09718adaf363c32c08624773492
The man <ThisIsntTheWay@users.noreply.github.com>
2021-05-28 20:31:55 +0200

    Add percentage change

--- bot.py
+++ bot.py
@@ -2,6 +2,8 @@ import requests
 import time
 import sqlite3
 import json
+import hmac
+import hashlib
 
 # =======================================================
 # ===       INIT                                      ===
@@ -28,20 +30,27 @@ with open('configuration.json') as config:
     data = json.load(config)
     
     # Populate ticker arrays
+    # Binance
     if (data["exchanges"]["binance"]["enabled"]):
      for i in range(len(data["exchanges"]["binance"]["tickers"])):
         tickersBinance.append(data["exchanges"]["binance"]["tickers"][i])
-        
+    
+    # Kraken
     if (data["exchanges"]["kraken"]["enabled"]):
      for i in range(len(data["exchanges"]["kraken"]["tickers"])):
         tickersKraken.append(data["exchanges"]["kraken"]["tickers"][i])
         
     # Read misc config
-    interval = data["dip_config"]["interval"]        
+    interval = data["dip_config"]["interval"]
+    dipThreshold = data["dip_config"]["threshold"]
+    dipThreshold = -dipThreshold
 
 # =======================================================
 # ===       FUNC                                      ===
 # =======================================================
+
+def hashing(query_string):
+    return hmac.new(secret.encode('utf-8'), query_string.encode('utf-8'), hashlib.sha256).hexdigest()
 
 def storeIntoDB(exchange, ticker, time, dip):
     # Store stuff into table
@@ -55,21 +64,26 @@ def getTime():
 class Binance:    
     # Acquire price    
     def getPrice(self, ticker):
-        response = requests.get(binanceAPI + str("avgPrice?symbol=") + str(ticker))
+        response = requests.get(binanceAPI + str("ticker/price?symbol=") + str(ticker))
         
         # Handle response
         if not response.status_code == 200:
             return "ERROR: " + response.status_code
         else:
             return response.json()['price']
-        
+
+# Object to use as a base price for all intervals
 class basePrice:
     def __init__(self, exchange, ticker, price):
-            self.exchange = exchange
-            self.ticker = ticker
-            self.price = price
+        self.exchange = exchange
+        self.ticker = ticker
+        self.price = price
+            
+    def report(self):
+        print(self)
+        
 
-# Create instances of classes
+# Create instances
 binance = Binance()
 
 # =======================================================
@@ -78,9 +92,10 @@ binance = Binance()
 
 # Acquire prices
 print(getTime() + " [INFO] Interval set at: " + str(interval) + " seconds.")
-print(getTime() + " [LOOP] Beginning monitor...")
+print(getTime() + " [LOOP] Beginning...")
 freshInterval = True
 intervalStart = int(time.time())
+haveBought = False
 
 while True:
     time.sleep(1)
@@ -88,6 +103,7 @@ while True:
     # Check if an interval has passed
     if (int(time.time()) - intervalStart) > interval:
         freshInterval = True
+        haveBought = False
         intervalStart = int(time.time())
         
     # Save price at start of interval
@@ -95,12 +111,29 @@ while True:
         print(getTime() + " [INFO] New interval!")
         
         # Populate intervalPrice[]
-        for i in range(len(tickersBinance)):
-            intervalPrice.append( basePrice('Binance', tickersBinance[i], binance.getPrice(tickersBinance[i])) )
+        if (data["exchanges"]["binance"]["enabled"]):
+            for i in range(len(tickersBinance)):
+                intervalPrice.append( basePrice('Binance', tickersBinance[i], int(float(binance.getPrice(tickersBinance[i])))) )
         
         freshInterval = False
+ 
+    #for obj in intervalPrice:
+    #    print( obj.exchange, obj.ticker, obj.price, sep =' ' )
         
-    for i in range(len(tickersBinance)):
-        print(tickersBinance[i] + " " + binance.getPrice(tickersBinance[i]))
-        
-        time.time()
+    # Always get the new prices
+    print(getTime() + " [PRIC] Querying binance...")
+    if (data["exchanges"]["binance"]["enabled"]):
+        for i in range(len(tickersBinance)):
+            priceNow = int(float(binance.getPrice(tickersBinance[i])))
+
+            # Check if the price is below the dip threshold
+            # formula to calculate percentage change: 100 * (NEW_PRICE - OLD_RPICE) / OLD_PRICE
+            for base in intervalPrice:
+                if base.exchange == "Binance" and base.ticker == tickersBinance[i]:
+                    percentage = 100 * (priceNow - base.price) / base.price
+                    print(getTime() + "   > " + tickersBinance[i] + ": " + str(priceNow) + " - change: " + str(round(percentage, 2)) + "%")
+                    
+                    # Buy if the price has dipped below threshold and nothing has been bought before
+                    if percentage < dipThreshold and not haveBought:
+                        print(getTime() + "       > Percentage below threshold, buying!")
+                        haveBought = True
