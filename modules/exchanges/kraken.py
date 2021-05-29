de77ad114b1e316f1315e83bc2a77b97208846aa
The man <ThisIsntTheWay@users.noreply.github.com>
2021-05-29 23:55:02 +0200

    Fix "Invalid key" on kraken POSTs
    I want to make it clear that the kraken API is an absolute shitshow to work with.
    It provides only cryptic errors and made me very angry.

--- modules/exchanges/kraken.py
+++ modules/exchanges/kraken.py
@@ -14,13 +14,16 @@ import modules.utils as utils
 import modules.configuration as modConfig
 import modules.discordBot as dBot
 
-krakenAPI = "https://api.kraken.com/0/"
+krakenAPI = "https://api.kraken.com"
+
+# ------------------------------
+#  Async functions
 
 async def krakenMonitor():
     await asyncio.sleep(8)
     
     while True:
-        await asyncio.sleep(3)
+        await asyncio.sleep(3)        
         print(utils.getTime() + " [PRIC] Querying kraken...")
         
         for i in range(len(modConfig.tickersKraken)):
@@ -47,6 +50,9 @@ async def krakenMonitor():
                             
                         base.bought = True
 
+# ------------------------------
+#  Functions
+
 def getSignature(urlpath, data, secret):
     postdata = urllib.parse.urlencode(data)
     encoded = (str(data['nonce']) + postdata).encode()
@@ -56,6 +62,14 @@ def getSignature(urlpath, data, secret):
     sigdigest = base64.b64encode(mac.digest())
     return sigdigest.decode()
 
+def kraken_request(uri_path, data, api_key, api_sec):
+    headers = {}
+    headers['API-Key'] = api_key
+    # get_kraken_signature() as defined in the 'Authentication' section
+    headers['API-Sign'] = getSignature(uri_path, data, api_sec)             
+    req = requests.post((krakenAPI + uri_path), headers=headers, data=data)
+    return req
+
 # ------------------------------
 #  Classes
 
@@ -63,7 +77,7 @@ class Kraken:
     # Acquire price
     def getPrice(self, ticker):
         # Use request with fiddler:  'proxies={"http": "http://127.0.0.1:8888", "https":"http:127.0.0.1:8888"}, verify=r"FiddlerRoot.pem"'
-        response = requests.get(krakenAPI + str("public/Ticker?pair=") + str(ticker))
+        response = requests.get(krakenAPI + str("/0/public/Ticker?pair=") + str(ticker))
         
         # Handle response
         if response.status_code == 200:
@@ -78,32 +92,15 @@ class Kraken:
     
     def buy(self, ticker, priceNow):
         stake = modConfig.data["exchanges"]["kraken"]["stake"]
-        URI = krakenAPI + "private/AddOrder"
         
-        # Assemble request body
-        requestBody = {
+        response = kraken_request('/0/private/AddOrder', {
             "nonce": str(int(1000*time.time())),
             "ordertype": "market",
             "type": "buy",
-            "volume": stake / priceNow,
+            "volume": str(stake / priceNow),
             "pair": ticker
-        }
-                #print(utils.getTime() + " [INFO] " + fullRequest)
-        
-        # Construct API signature
-        signature = getSignature(URI, requestBody, modConfig.data["exchanges"]["kraken"]["api_secret"])
-        
-        # Send POST
-        requestHeaders = {
-            'API-Key': str(modConfig.data["exchanges"]["kraken"]["api_key"]),
-            'API-Sign': signature,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }        
-        # URL-encode request body
-        #requestBody = urllib.parse.urlencode(requestBody)
-        
-        response = requests.post(URI, headers=requestHeaders, data=requestBody)
-        
+        }, modConfig.data["exchanges"]["kraken"]["api_key"], modConfig.data["exchanges"]["kraken"]["api_secret"])
+
         # Handle response
         # > Because kraken is against standards, its API will NOT return HTTP/4xx on an error
         #   As such, we need to check if error[] is empty or not
