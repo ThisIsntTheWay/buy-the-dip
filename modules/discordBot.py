4d603b9ce44bac27eda36033c92cff5df85b7961
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-08 22:36:31 +0200

    Update discordBot.py

--- modules/discordBot.py
+++ modules/discordBot.py
@@ -1,37 +1,43 @@
 # Discord bot module
 
 import discord
-import time
-import json
+import asyncio
+from discord.ext.commands import Bot
 
 import modules.utils as utils
 import modules.configuration as config
     
 discordActive = config.data["discord"]["enabled"]
 
-# Bot class
-class SniperGuy(discord.Client):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-    async def on_ready(self):
-        print(utils.getTime() + ' [BOT ] Login was successful: ' + self.user.name + ' / ' + str(self.user.id))
-        
-    async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
+client = Bot('!')
 
-        if message.content.startswith('!ping'):
-            await message.reply('Pong', mention_author=True)
-            
-    async def send_message(self, message):
-        channel = self.get_channel(int((config.data["discord"]["channel"])))
-        await channel.send(message)
+@client.event
+async def on_ready():
+    print(utils.getTime() + ' [BOT] Successful login as {0.user}'.format(client))
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-client = SniperGuy()
+    if message.content.startswith('!ping'):
+        await message.reply('Pong', mention_author=True)
+
+@client.command(pass_context = True)
+async def clear(ctx, number):
+    print("clear called")
+    number = int(number) #Converting the amount of messages to delete to an integer
+    counter = 0
+    async for x in client.logs_from(ctx.message.channel, limit = number):
+        if counter < number:
+            await client.delete_message(x)
+            counter += 1
+            await asyncio.sleep(1.2) #1.2 second timer so the deleting process can be even
+
         
 # Function to send a discord bot message from external modules
 # Required because a circular import would fuck things up
-async def sendMsgByProx(msg):
+async def sendMsgByProxy(msg):
     if discordActive:
-        await client.send_message(msg)
+        channel = client.get_channel(int((config.data["discord"]["channel"])))
+        await channel.send(msg)
