28cd0a80653f5ad7ad3bf9913946905e68b4ec88
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-09 10:51:28 +0200

    Implement permissions

--- modules/discordBot.py
+++ modules/discordBot.py
@@ -3,6 +3,8 @@ import time
 import asyncio
 
 from discord.ext.commands import Bot
+from discord.ext.commands import MissingPermissions
+from discord.ext.commands import has_permissions
 
 import modules.utils as utils
 import modules.configuration as modConfig
@@ -12,6 +14,7 @@ percentageBuffer = []
 
 client = Bot('!')
 
+# Events
 @client.event
 async def on_ready():
     print(utils.getTime() + ' [BOT] Successful login as {0.user}'.format(client))
@@ -20,12 +23,14 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content.startswith('!ping'):
-        await message.reply('Pong', mention_author=True)
     
     # Permit processing of @client.command() stuff
     await client.process_commands(message)
+    
+# Commands
+@client.command(name='ping', help='Have the bot confirm it\'s still alive.', pass_context=True)
+async def ping(ctx):
+    await ctx.reply("Pong!", mention_author=True)
 
 @client.command(name='percentage', help='Get price change as percentage of specified ticker.')
 async def percentage(ctx, ticker=None):
@@ -47,13 +52,18 @@ async def timeleft(ctx):
     await ctx.send(msg)
 
 @client.command(name='nuke', help='Completely nuke text channel.')
+@has_permissions(manage_messages=True)  
 async def nuke(ctx):
     await ctx.channel.purge(limit=600)
     await ctx.send('Done purging this channel.')
+    
+@nuke.error
+async def kick_error(error, ctx):
+   if isinstance(error, MissingPermissions):
+       await ctx.send("You are not authorized to use this command.")
 
-        
-# Function to send a discord bot message from external modules
-# Required because a circular import would fuck things up
+# Function to send a message using the discord bot from external modules
+# Required because a circular import would fuck things up otherwise
 async def sendMsgByProxy(msg):
     if discordActive:
         channel = client.get_channel(int((modConfig.data["discord"]["channel"])))
