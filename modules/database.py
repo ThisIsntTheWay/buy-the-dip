982de097dc1df71021c7cb9ab5a4ea03e96a5e75
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-09 16:07:55 +0200

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -5,9 +5,9 @@ import sqlite3
 database = sqlite3.connect('./botRecords.db')
 dbMutation = database.cursor()
 
-def storeIntoDB(exchange, ticker, time, dip, note):
+def storeIntoDB(exchange, ticker, time, dip, timeframe, note):
     # Store stuff into table
-    dbMutation.execute("INSERT INTO history VALUES ('" + exchange + "', '" + ticker + "', '" + time + "', '" + dip + "' + '" + note + "')")
+    dbMutation.execute("INSERT INTO history VALUES ('" + exchange + "', '" + ticker + "', '" + time + "', '" + dip + "', '" + timeframe + "', '" + note + "')")
     dbMutation.commit()
     
 # Check if table exists
@@ -15,5 +15,5 @@ dbMutation.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' 
 if dbMutation.fetchone()[0]==1:
     print('[INIT] Table exists.')
 else:
-    dbMutation.execute('''CREATE TABLE history ([ID] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, [exchange] TEXT NOT NULL, [ticker] TEXT NOT NULL, [time] TEXT NOT NULL, [dip] INTEGER, [result] TEXT)''')
+    dbMutation.execute('''CREATE TABLE history ([ID] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, [exchange] TEXT NOT NULL, [ticker] TEXT NOT NULL, [time] TEXT NOT NULL, [timeframe] INTEGER, [dip] INTEGER, [result] TEXT)''')
     print('[INIT] Table created.')
