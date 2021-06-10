00c1eb7f1b395ec6e51b8a0afe366292527b0be4
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-10 22:36:24 +0200

    Properly escape SQL statement

--- modules/database.py
+++ modules/database.py
@@ -7,7 +7,7 @@ dbMutation = database.cursor()
 
 def storeIntoDB(exchange, ticker, time, dip, timeframe, note):
     # Store stuff into table
-    dbMutation.execute("INSERT INTO history VALUES (NULL, '" + exchange + "', '" + ticker + "', '" + time + "', '" + str(dip) + "', '" + str(timeframe) + "', '" + note + "')")
+    dbMutation.execute('INSERT INTO history VALUES (NULL, ?, ?, ?, ?, ?, ?)', (exchange, ticker, time, str(dip), str(timeframe), note))
     database.commit()
     
 # Check if table exists
