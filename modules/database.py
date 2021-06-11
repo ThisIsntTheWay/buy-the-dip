c3ca11ab01f5817f19658620575398081cbf474c
The man <ThisIsntTheWay@users.noreply.github.com>
2021-06-11 11:55:54 +0200

    Change print() to utils.log()

--- modules/database.py
+++ modules/database.py
@@ -1,6 +1,7 @@
 # Database module
 
 import sqlite3
+import modules.utils as utils
 
 database = sqlite3.connect('./botRecords.db')
 dbMutation = database.cursor()
@@ -13,7 +14,7 @@ def storeIntoDB(exchange, ticker, time, dip, timeframe, note):
 # Check if table exists
 dbMutation.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='history' ''')
 if dbMutation.fetchone()[0]==1:
-    print('[INIT] Table exists.')
+    utils.log('[INIT] Table exists.')
 else:
     dbMutation.execute('''CREATE TABLE history ([ID] INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE, [exchange] TEXT NOT NULL, [ticker] TEXT NOT NULL, [time] TEXT NOT NULL, [timeframe] INTEGER, [dip] INTEGER, [result] TEXT)''')
-    print('[INIT] Table created.')
+    utils.log('[INIT] Table created.')
