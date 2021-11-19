b6fe1fcdb84252007a0c392a5c5a184f0ddd1344
The Man <ThisIsntTheWay@users.noreply.github.com>
2021-11-19 18:26:08 +0100

    Add DNS check

--- modules/utils.py
+++ modules/utils.py
@@ -1,9 +1,17 @@
 # Various auxilliary functions
 
 import time
+import socket
 
 def getTime():
     return time.strftime("%H:%M:%S", time.localtime())
 
 def log(msg):
     print(getTime() + " " + msg)
+
+def hostname_resolves(hostname):
+    try:
+        socket.gethostbyname(hostname)
+        return True
+    except socket.error:
+        return False
